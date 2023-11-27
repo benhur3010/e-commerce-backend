12b28c0aac4391b5751e14be63830bdad3c82d3e
Benhur3010 <benhurbueno.dev@gmail.com>
2023-11-27 13:35:16 -0300

    feat: user service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,12 @@
 import { CreateUserDto } from './dtos/createUser.dto';
 import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly userService: UserService) {}
   @Post()
   async createUser(@Body() createUser: CreateUserDto) {
-    return {
-      ...createUser,
-      password: undefined,
-    };
+    return this.userService.createUser(createUser);
   }
 }
