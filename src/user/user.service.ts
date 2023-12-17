0169db2a7b3d7c7c65e48b1ad6467c9dabb17a7e
Benhur3010 <benhurbueno.dev@gmail.com>
2023-12-16 22:23:09 -0300

    feat: validation in address

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { hash } from 'bcrypt';
 import { UserEntity } from './entities/user.entity';
@@ -26,5 +26,17 @@ export class UserService {
 
   async getAllUser(): Promise<UserEntity[]> {
     return this.userRepository.find();
+  }
+
+  async findUserById(userId: number): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      where: {
+        id: userId,
+      },
+    });
+    if (!user) {
+      throw new NotFoundException('UserId Not Found');
+    }
+    return user;
   }
 }
