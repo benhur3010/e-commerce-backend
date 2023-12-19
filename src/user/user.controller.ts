4224c29e663dc7db366bb9317f8da6d2010a1a22
Benhur3010 <benhurbueno.dev@gmail.com>
2023-12-19 00:05:15 -0300

    feat: relations many-to-one

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -30,7 +30,9 @@ export class UserController {
   }
 
   @Get('/:userId')
-  async getUserById(@Param('userId') userId: number) {
-    return this.userService.getUserByIdUsingRelations(userId);
+  async getUserById(@Param('userId') userId: number): Promise<ReturnUserDto> {
+    return new ReturnUserDto(
+      await this.userService.getUserByIdUsingRelations(userId),
+    );
   }
 }
