1b0de65f902b3482f58fe06e23c9002d558a3128
Benhur3010 <benhurbueno.dev@gmail.com>
2023-12-24 17:56:55 -0300

    feat: validation in email duplicated

---
+++ src/migration/1703450130251-alter-table-user.ts
@@ -0,0 +1,15 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AlterTableUser1703450130251 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+        alter table public.user add unique(email);
+      `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.query(`
+        
+      `);
+  }
+}
